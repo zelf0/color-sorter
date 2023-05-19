c7836f16f67d2ece0ba00e0cec192c2b84db20b8
Esther Feldman <estherfeldman7@gmail.com>
2023-05-19 18:58:17 -0400

    format document

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import Tile from './Tile';
+import React from "react";
+import Tile from "./Tile";
 
-const Board = ({board, swapTile}) => {
-  return <div>
-    {board.map((row, i) => 
-        <div className = "row" key = {`row-${i}`}> 
-        {row?.map((tile, j) => 
-
-                // <div style = {{backgroundColor: `rgb(${tile.red}, ${tile.green}, ${tile.blue})`}} className = "tile" key = {`tile-${i}-${j}`}> 
-                //     {/* {`rgb(${tile.red}, ${tile.green}, ${tile.blue})`} */}
-                // </div>
-                <Tile index = {{row: i, column: j}} swapTile = {swapTile} target = {`rgb(${tile?.target?.red}, ${tile?.target?.green}, ${tile?.target?.blue})`} color = {`rgb(${tile?.value?.red}, ${tile?.value?.green}, ${tile?.value?.blue})`} key = {`tile-${i}-${j}`}/>
-        )}
+const Board = ({ board, swapTile }) => {
+  return (
+    <div>
+      {board.map((row, i) => (
+        <div className="row" key={`row-${i}`}>
+          {row?.map((tile, j) => (
+            <Tile
+              index={{ row: i, column: j }}
+              swapTile={swapTile}
+              target={`rgb(${tile?.target?.red}, ${tile?.target?.green}, ${tile?.target?.blue})`}
+              color={`rgb(${tile?.value?.red}, ${tile?.value?.green}, ${tile?.value?.blue})`}
+              key={`tile-${i}-${j}`}
+            />
+          ))}
         </div>
-    )}
-
-  </div>;
+      ))}
+    </div>
+  );
 };
 
 export default Board;
