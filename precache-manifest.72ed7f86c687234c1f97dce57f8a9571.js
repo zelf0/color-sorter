2c3abfea6fa8ec967cca0478dc69b7f1d8bb7d02
Esther Feldman <estherfeldman7@gmail.com>
2023-05-23 21:12:28 -0400

    Updates

---
+++ precache-manifest.72ed7f86c687234c1f97dce57f8a9571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c69db08b05f10185784e",
+    "url": "/color-sorter/static/css/main.d5b216dd.chunk.css"
+  },
+  {
+    "revision": "c69db08b05f10185784e",
+    "url": "/color-sorter/static/js/main.46d31dd9.chunk.js"
+  },
+  {
+    "revision": "6ea53c719490380584f1",
+    "url": "/color-sorter/static/js/runtime~main.4656b383.js"
+  },
+  {
+    "revision": "3f0bea2a040d95157e6e",
+    "url": "/color-sorter/static/js/2.aa5d3d5e.chunk.js"
+  },
+  {
+    "revision": "faf110be1a62be431a1f",
+    "url": "/color-sorter/static/js/3.7616fc4b.chunk.js"
+  },
+  {
+    "revision": "1385440444c2ea168eb1568c2143cc2b",
+    "url": "/color-sorter/index.html"
+  }
+];
